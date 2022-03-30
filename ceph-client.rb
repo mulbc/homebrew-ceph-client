16a060b8c1b233b7169ba3bcd4727f2ba64cf2d9
Mouratidis Theofilos <mtheofilos@gmail.com>
2022-03-30 14:09:12 +0200

    add pkgconfig for macfuse v4+ (prev osxfuse)

--- ceph-client.rb
+++ ceph-client.rb
@@ -37,6 +37,7 @@ class CephClient < Formula
   def install
     ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["nss"].opt_lib}/pkgconfig"
     ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl"].opt_lib}/pkgconfig"
+    ENV.prepend_path "PKG_CONFIG_PATH", "/usr/local/lib/pkgconfig"
     xy = Language::Python.major_minor_version "python3"
     ENV.prepend_create_path "PYTHONPATH", "#{Formula["cython"].opt_libexec}/lib/python#{xy}/site-packages"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
