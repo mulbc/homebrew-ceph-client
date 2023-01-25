a13f8191c862e29ee4c2703a88952fd2190f8fcd
Alex Bowley <alex@Mac-Pro.local>
2023-01-25 16:32:15 +0200

    Downgrade boost dependency to below 1.81, to avoid boost::phoenix::placeholder::uargX duplicate symbol errors

--- ceph-client.rb
+++ ceph-client.rb
@@ -12,7 +12,7 @@ class CephClient < Formula
   end
 
   # depends_on "osxfuse"
-  depends_on "boost"
+  depends_on "boost@1.76"
   depends_on "openssl" => :build
   depends_on "cmake" => :build
   depends_on "ninja" => :build
